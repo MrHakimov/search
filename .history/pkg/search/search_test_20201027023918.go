5d81ea1b4c9994ba9dbea41b62cf025e986b4bce
Muhammadjon Hakimov <hakimov0777@gmail.com>
2020-10-27 02:39:44 +0300

    Renamed module

---
+++ .history/pkg/search/search_test_20201027023918.go
@@ -0,0 +1,19 @@
+package search
+
+import (
+	"context"
+	"fmt"
+	"testing"
+)
+
+func TestAll_user(t *testing.T) {
+	fmt.Println("Started testing...")
+
+	ch := All(context.Background(), "ipsum", []string{"../../data/data.txt"})
+
+	_, err := <-ch
+
+	if !err {
+		t.Error(err)
+	}
+}
